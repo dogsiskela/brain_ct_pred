5afb10cb4f8f94c98401d258208015660767eb8e
Aleks <aleksandra.popovska@outlook.com>
2024-09-25 22:07:50 +0200

    fix len models

--- brain_ct/src/models/models.py
+++ brain_ct/src/models/models.py
@@ -7,15 +7,10 @@ from segmentation_models import get_preprocessing
 from segmentation_models.metrics import iou_score
 from segmentation_models import Unet
 
-
-# import tf_keras
-
 BACKBONE = "densenet121"
 preprocess_input = get_preprocessing(BACKBONE)
 
-# Note that the model takes 3-channel images as input
 model = Unet(BACKBONE, input_shape=(128, 128, 3),
-             # freeze_encoder=False,
              classes=1,
              encoder_weights='imagenet',
              activation='sigmoid')
@@ -23,12 +18,12 @@ model = Unet(BACKBONE, input_shape=(128, 128, 3),
 model.compile(optimizer='adam', loss='binary_crossentropy',
               metrics=[iou_score])
 
-filepath = "model_aug.h5"
+filepath = "model_no_aug.h5"
 
 model.load_weights('./'+filepath)
 
 
 def predict(test_generator, data_len):
     predictions = model.predict(test_generator,
-                                steps=len(data_len),
+                                steps=data_len,
                                 verbose=1)
